a6d385a335c148d794a8414ebe1771a4c3505fe1
bharat-hyd <bharathteamworks@gmail.com>
2025-10-10 10:53:12 +0530

    make it mobile responsive

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,46 @@
+import { useEffect, useState } from 'react';
 import logo from './assets/web-logo.svg'
+
 function Navbar(){
+    const [scroll, setScroll] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 40){
+                setScroll(true)
+            }else{
+                setScroll(false)
+            }
+        }
+        window.addEventListener("scroll",handleScroll)
+        return () => window.removeEventListener("scroll",handleScroll)
+    })
+
+
+// mobile nav bar
+
+const [menu,setMenu] = useState(false)
+
+const toggleMenu = () => {
+    setMenu(prev => !prev);
+}
+
+    const hero_h1 = ["Fun","Quality","Family"]
+    const [hIndex,setHIndex] = useState(0)
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setHIndex((prev) => prev + 1 >= hero_h1.length ? 0 : prev + 1)
+        },3000)
+        return () => clearInterval(timer)
+    },[])
     return(
         <>
         <style>
             {
                 `
                 nav{
-                    background:rgba(0,0,0,0.3);
-                    padding:10px 10px;
+                    background:rgba(0,0,0,0);
+                    padding:15px 10px;
                     position:fixed;
                     top:0;
                     left:0;
@@ -40,10 +73,28 @@ function Navbar(){
                     margin-right:20px;
                     color:#22438a;
                 }
+                
+                .scrolled {
+                    background:white;
+                    padding:14px 10px;
+                    box-shadow: 0px 1px 10px #dfdfdf;
+                    transition: background 1.0s ease, padding 1.0s ease 0.4s;
+                }
+                    
+                .scrolled li a{
+                    color:#22438a;
+                    font-size:18px;
+                    padding:5px 15px;
+                }
+                @media(max-width:768px){
+                    nav{
+                        display:none;
+                    }
+                }
                 `
             }
         </style>
-        <nav>
+        <nav className={scroll ? 'scrolled' : ''}>
             <div className="container">
                 <div className="nav-list">
                     <img src={logo} className='logo'/>
@@ -61,16 +112,165 @@ function Navbar(){
                 </div>
             </div>
         </nav>
+        <style>
+            {
+                `
+                #mobile-nav{
+                    position:fixed;
+                    top:0;
+                    left:0;
+                    width:100%;
+                    z-index:1000;
+                    padding:10px;
+                    box-sizing: border-box;
+                    display:none;
+                }
+                .mobile-head{
+                    display:flex;
+                    flex-wrap:wrap;
+                    justify-content:space-between;
+                    align-items:center;
+                }
+                .mobile-head.active {
+                    border-bottom:1px solid #a8c6eb;
+                }
+                .mobile-head .logo {
+                    width:120px;
+                }
+                .humburg {
+                    width: 45px;
+                    height: 30px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    cursor: pointer;
+                    padding: 10px;
+                }
+
+                .humburg span {
+                    display: block;
+                    height: 4px;
+                    width: 100%;
+                    background: white;
+                    border-radius: 2px;
+                    transition: all 0.3s ease;
+                    transition-origin:center;
+                }
+
+                .humburg.open span:nth-child(1){ 
+                transform: rotate(45deg) translate(9px, 9px);
+                }
+
+                .humburg.open span:nth-child(2){
+                    opacity: 0;
+                }
+
+                .humburg.open span:nth-child(3){
+                transform: rotate(-45deg) translate(9px, -9px);
+                }
+                .mobile-menu{
+                    text-align:center;
+                }
+                .mobile-menu-list{
+                    list-style:none;
+                }
+                .mobile-menu-list li{
+                    padding:10px;
+                    margin-bottom:10px;
+                }
+                .mobile-menu-list li a{
+                    color:white;
+                }
+                .mobile-menu-button-list{
+                    list-style:none;
+                }
+                .mobile-menu-button-list li{
+                    padding:10px;
+                    margin-bottom:20px;
+                }
+                .mobile-menu-button-list li a{
+                    color:#22438a;
+                    background:white;
+                    padding:12px 17px;
+                }
+                
+                .nav-overlay{
+                    position:absolute;
+                    top:0;
+                    right:0;
+                    background:#22438a;
+                    z-index:-1;
+                    animation:backGround 0.5s ease forwards;
+                }
+                @keyframes backGround{
+                    0%{
+                        width:80px;
+                        height:80px;
+                        border-bottom-left-radius:50%;
+                    }
+                    100%{
+                        width:100%;
+                        height:100vh;
+                        border-bottom-left-radius:0%;
+                    }
+                }
+                @media(max-width:768px){
+                    #mobile-nav{
+                        display:block;
+                    }
+                }
+                `
+            }
+        </style>
+        <section id="mobile-nav">
+            <div className={`mobile-head ${menu ? 'active' : ''}`}>
+                <img src={logo} className='logo'/>
+                <div className={`humburg ${menu ? 'open' : ''}`} onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            {menu && 
+            <>
+                <div className='mobile-menu'>
+                    <ul className='mobile-menu-list'>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Foundation</a></li>
+                        <li><a href="">Careers</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Faq</a></li>
+                    </ul>
+                    <ul className='mobile-menu-button-list'>
+                        <li><a href="">Enquiry</a></li>
+                        <li><a href="">WhatsApp</a></li>
+                    </ul>
+                </div>
+                <div className='nav-overlay'></div>
+            </>
+             }
+        </section>
             <style>
             {
                 ` #hero {
                     height: 100vh;
                     position:relative;
                     }
+                #hero::after {
+                    position:absolute;
+                    top:0;
+                    left:0;
+                    width:100%;
+                    height:100%;
+                    z-index:1;
+                    background: radial-gradient(872px at 24% 2%, rgba(255, 153, 0, 0.5) 0px, rgba(0, 52, 113, 0.5) 100%);
+                    content:'';
+                }
                 .main-video {
                     width: 100%;
                     height: 100%;
                     object-fit: cover; /* fills hero section nicely */
+                    position:relative;
                 }
                 .hero-overlay{
                     position:absolute;
@@ -83,7 +283,7 @@ function Navbar(){
                 }
                 
                 .hero-overlay h1{
-                    font-family: "Times New Roman", Times;
+                    font-family: "Playfair Display", serif;
                     font-size:54px;
                     line-height:2.5;
                     border-top:1px solid #ccc;
@@ -164,6 +364,42 @@ function Navbar(){
                         opacity:0;
                     }
                 }
+                .hero_change_wrapper {
+                    display: inline-block;
+                }
+                .hero_change{
+                    display: inline-block;
+                    animation:slideDown1 0.4s ease forwards, slideDown2 0.4s ease 2.5s forwards;
+                }
+                @keyframes slideDown1{
+                    0%{
+                        opacity:1;
+                        transform:translateY(-30px);
+                    }
+                    100%{
+                        opacity:1;
+                        transform:translateY(0px);
+                    }
+                }
+                @keyframes slideDown2{
+                    0%{
+                        opacity:1;
+                        transform:translateY(0px);
+                    }
+                    100%{
+                        opacity:0;
+                        transform:translateY(30px);
+                    }
+                }
+                    @media(max-width:768px){
+                    .hero-overlay h1 {
+                        font-size: 30px;
+                    }
+                    .hero-overlay p {
+                        font-size: 18px;
+                        line-height: 1.8;
+                    }
+                    }
                 `
             }
         </style>
@@ -172,7 +408,13 @@ function Navbar(){
 				<source src="https://psgroup.in/assets/media/home-video.mp4" type="video/mp4" />
 			</video>
             <div className='hero-overlay'>
-                <h1>Its <span className='head'><b className='visible'>Fun</b><b className='hide'>Quality</b><b className='hide'>Family</b></span> Time</h1>
+                <h1>Its 
+                      {" "}<span className="hero_change_wrapper">
+                        <span key={hIndex} className="hero_change">
+                        {hero_h1[hIndex]}
+                        </span>
+                    </span>{" "}
+                     Time</h1>
                 <p><span className='num'>15000+</span>  Happy Families</p>
             </div>
             <div className='hero-overlay-btn'>
